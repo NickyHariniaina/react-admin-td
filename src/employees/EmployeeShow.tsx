a2ad6d916f5bed0f31e1dd79e59bf29b1f0257cf
NickyHariniai <ferrissushi@gmail.com>
2026-05-31 17:43:49 +0300

    fix(employees): align EmployeeShow source fields with db.json keys

--- src/employees/EmployeeShow.tsx
+++ src/employees/EmployeeShow.tsx
@@ -1,32 +1,34 @@
 import {
-    BooleanField,
-    EditButton,
-    ListButton,
-    NumberField,
-    Show,
-    SimpleShowLayout,
-    TextField,
-    TopToolbar,
-} from "react-admin"
+  BooleanField,
+  EditButton,
+  ListButton,
+  NumberField,
+  Show,
+  SimpleShowLayout,
+  TextField,
+  TopToolbar,
+} from "react-admin";
 
 const ShowActions = () => (
-    <TopToolbar>
-        <ListButton />
-        <EditButton />
-    </TopToolbar>
-)
+  <TopToolbar>
+    <ListButton />
+    <EditButton />
+  </TopToolbar>
+);
 
 export const EmployeeShow = () => {
-    return <Show actions={<ShowActions />}>
-        <SimpleShowLayout>
-            <TextField source="prenom" />
-            <TextField source="email" />
-            <TextField source="departement" />
-            <NumberField
-                source="salaire"
-                options={{ style: "currency", currency: "EUR" }}
-            />
-            <BooleanField source="actif" />
-        </SimpleShowLayout>
+  return (
+    <Show actions={<ShowActions />}>
+      <SimpleShowLayout>
+        <TextField source="firstname" />
+        <TextField source="email" />
+        <TextField source="department" />
+        <NumberField
+          source="salary"
+          options={{ style: "currency", currency: "EUR" }}
+        />
+        <BooleanField source="isActive" />
+      </SimpleShowLayout>
     </Show>
-}
+  );
+};
